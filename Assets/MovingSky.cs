a9455a1453da77e3b6c5bc31ee45f0da1b78da82
ayataka <scene.of.bloodshed232@gmail.com>
2015-11-30 20:33:49 +0900

    ・スカイボックスを動かした ・ネイルアートを変更

---
+++ Assets/MovingSky.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingSky : MonoBehaviour {
+
+    public static float axis;
+
+	// Use this for initialization
+	void Start () {
+        axis = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+        transform.Rotate(new Vector3(0, 0, axis + 0.05f));
+	}
+}
